13fea0e593865ce1f4490d266403a320c4bf8cd7
Calvin Tallent <calvintallentspam@gmail.com>
2026-02-04 11:37:08 -0800

    Set up building for github pages.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+const dev = process.argv.includes('dev');
+
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	base: '/'
+	// base: dev ? '' : '/portfolio/'
 });
